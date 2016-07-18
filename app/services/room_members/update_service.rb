3f4f6f337627dac67450eb1bc0ecff7a5504f1a4
Yu Totsuka <totutote@users.noreply.github.com>
2016-07-18 17:00:10 +0900

    参加メンバーの編集機能追加

---
+++ app/services/room_members/update_service.rb
@@ -0,0 +1,17 @@
+module RoomMembers
+  class UpdateService < BaseService
+    def initialize(player_params, room_member_params, room_member)
+      @player_params = player_params.dup
+      @room_member_params = room_member_params.dup
+      @room_member = room_member
+    end
+
+    def execute
+      @room_member.acceptable_party_leader = @room_member_params["acceptable_party_leader"]
+      @room_member.acceptable_room_leader = @room_member_params["acceptable_room_leader"]
+      @room_member.save
+      @room_member.player.player_id_name = @player_params["player_id_name"]
+      @room_member.player.save
+    end
+  end
+end
